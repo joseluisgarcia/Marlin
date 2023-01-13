9167b277df5095ffc9e6da38e2fbe6b9e1a45234
José Luis García Morillo <programacho@gmail.com>
2023-01-13 11:08:59 +0100

    Correccion de pasos del eje Z, a falta de cambiar E0 con E1 por no funcionar el extrusor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1165,7 +1165,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 415 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 403, 415 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
